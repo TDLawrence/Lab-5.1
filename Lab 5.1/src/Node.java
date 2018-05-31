58c6b469caa0141f3a487254d86d9b56a25555c6
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-05-31 09:08:52 -0400

    Welcome

---
+++ Lab 5.1/src/Node.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+public class Node {
+ArrayList<Node>children;
+
+public Node()
+{this.children=children;	
+}
+
+public ArrayList<Node> getChildren()
+{ArrayList<Node>childNodes=new ArrayList<Node>;
+ int x=children.size();
+ for (int y=0;y<x;y++)
+  {if(this.children.get(y)!=null)
+   {childNodes.add(this.children.get(y));
+	  
+   }
+	 
+  }
+	
+}
+
+}
